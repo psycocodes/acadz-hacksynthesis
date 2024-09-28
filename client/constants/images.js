9ae91dfc2cead8cb1c85c3a6a033c7f2e6cd498f
Mohikshit Ghorai <gmohikshit@gmail.com>
2024-09-28 19:02:16 +0530

    added Gitignore and Scripts dir

--- client/constants/images.js
+++ client/constants/images.js
@@ -5,5 +5,7 @@ import path from "../assets/images/path.png";
 import logo from "../assets/images/logo.png";
 import logoSmall from "../assets/images/logo-small.png";
 import empty from "../assets/images/empty.png";
+import notebookCreate from "../assets/images/notebook-create.png";
+import groupCreate from "../assets/images/group-create.png";
 
-export default { profile, thumbnail, cards, path, logo, logoSmall, empty };
+export default { profile, thumbnail, cards, path, logo, logoSmall, empty, notebookCreate, groupCreate };
