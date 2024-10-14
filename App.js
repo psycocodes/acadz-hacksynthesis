aa10186e1d63e5fd897ed275dda41019d449ed65
frank06n <dev.code38@gmail.com>
2024-10-15 01:11:03 +0530

    test screen removed

--- App.js
+++ App.js
@@ -21,7 +21,6 @@ import YoutubeSuggestionsScreen from './screens/products/YtSuggestions';
 
 import FlashcardSessionScreen from './screens/fsessions/Session';
 import FlashcardSessionResultScreen from './screens/fsessions/Result';
-import TestScreenAudio from './screens/TestScreenAudio';
 
 const Stack = createStackNavigator();
 const DefaultTheme = MD3DarkTheme;
@@ -66,8 +65,6 @@ export default function App() {
 
                     <Stack.Screen name="FlashcardSession" component={FlashcardSessionScreen} />
                     <Stack.Screen name="FlashcardSessionResult" component={FlashcardSessionResultScreen} />
-
-                    <Stack.Screen name="TestAudio" component={TestScreenAudio} />
 
                 </Stack.Navigator>
             </NavigationContainer>
